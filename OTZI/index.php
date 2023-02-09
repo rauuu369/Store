da11f3e4ad3ef6a13d7009764008241f3cf04c03
rauuu369 <rauuuurauuuu@gmail.com>
2023-02-09 13:59:03 -0800

    Se agrega la pagina index.php y index.css

---
+++ OTZI/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="icon" href="images/Logo.jpg">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300&display=swap" rel="stylesheet">
+    <title>About Us - OTZI</title>
+</head>
+<body>
+    <div class="initial_clothing">
+        
+    </div>
+    <div class="about">
+        <h3>Our family of brands and businesses is dedicated to making it easy for customers around the world to express their unique style through fashion and design. With a wide variety of products from our own in-house lines, as well as collaborations with leading designers, we have something for everyone.</h3>
+    </div>
+</body>
+</html>

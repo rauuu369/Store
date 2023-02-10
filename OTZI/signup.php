5bddcd4bb529ab4e624a8fbb34236996731efc94
rauuu369 <rauuuurauuuu@gmail.com>
2023-02-10 12:58:16 -0800

    Se agrega el archivo signup.css y se agrega el archivo signup.php

---
+++ OTZI/signup.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/signup.css">
+    <link rel="icon" href="images/Logo.jpg">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
+    <title>Sign Up - OTZI</title>
+</head>
+<body>
+    
+</body>
+</html>

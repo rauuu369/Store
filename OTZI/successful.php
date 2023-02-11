69e82ee41008c278ea10a0102c78151611631ede
rauuu369 <rauuuurauuuu@gmail.com>
2023-02-10 19:01:53 -0800

    Se agrega la pagina successful y su estilo

---
+++ OTZI/successful.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/successful.css">
+    <link rel="icon" href="images/Logo.jpg">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
+    <title>Sign Up - OTZI</title>
+</head>
+<body>
+    <div class="container">
+    <div class="image_signup">
+        <img src="images/signup.png" width=50% height=850px >
+    </div>
+    <div class="welcome_signup">
+        <div class="sign_up">
+            <h1>Create Your Account</h1>
+        </div>
+        <div class="question">
+            <h2>Enter the fields  below to get started.</h2>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
